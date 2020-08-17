9d8a49a54fe70572f36e44d9e9ef66f75badef70
Adam Cassidy <adamcassidy011@gmail.com>
2020-08-17 06:20:31 -0400

    Deleted boilerplate, imported icons, and added dependencies.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-toolbar>
+      <v-toolbar-title>Feedback</v-toolbar-title>
+      <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav"></v-navigation-drawer>
+
+    <main></main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
