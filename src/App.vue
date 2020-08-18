21a24a945de1c3fac0bac7912f422ae8190ccdd5
Adam Cassidy <adamcassidy011@gmail.com>
2020-08-18 10:19:16 -0400

    Changed sideNav to openDrawer and added list items and icons to the drawer.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <v-app>
     <v-toolbar>
-      <v-app-bar-nav-icon @click="sideNav = !sideNav"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="openDrawer = !openDrawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Feedback</v-toolbar-title>
     </v-toolbar>
-    <v-navigation-drawer v-model="sideNav"></v-navigation-drawer>
+    <v-navigation-drawer v-model="openDrawer">
+      <v-list flat>
+        <v-list-item-group>
+          <v-list-item v-for="item in drawerItems" :key="item.key">
+            <v-list-item-content>
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ item.title}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <main></main>
   </v-app>
@@ -16,7 +29,12 @@ export default {
 
   data: () => {
     return {
-      sideNav: false
+      openDrawer: false,
+      drawerItems: [
+        { icon: "house", title: "Home Projects" },
+        { icon: "sports_basketball", title: "Sports" },
+        { icon: "restaurant", title: "Meal" }
+      ]
     };
   }
 };
