1a418ec57d38a44cb906681a06233e3c08a0ac06
Adam Cassidy <adamcassidy011@gmail.com>
2020-08-21 10:16:05 -0400

    Added Firebase configuration information to main.js

--- src/main.js
+++ src/main.js
@@ -12,7 +12,15 @@ new Vue({
   router,
   render: (h) => h(App),
   created() {
-    var firebaseConfig = {};
+    var firebaseConfig = {
+      apiKey: "AIzaSyDFQWaigSu1k-T2RVc7oOfKTJ4EmsLlwws",
+      authDomain: "feedback-project-20f04.firebaseapp.com",
+      databaseURL: "https://feedback-project-20f04.firebaseio.com",
+      projectId: "feedback-project-20f04",
+      storageBucket: "feedback-project-20f04.appspot.com",
+      messagingSenderId: "624112482142",
+      appId: "1:624112482142:web:776f42503fa6f7d41d01cf",
+    };
     firebase.initializeApp(firebaseConfig);
   },
 }).$mount("#app");
