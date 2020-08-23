28bff4ce4dbcbece69988fc2802eff46a5ffebd9
Adam Cassidy <adamcassidy011@gmail.com>
2020-08-23 08:07:52 -0400

    Add vuex store and modified Sign-up.vue
    Change v-col to v-flex and changed method 'addToStore' to'addToUsers' in
    Sign-up.vue. Add boiilerplate store/index.js.

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import VueX from "vuex";
+
+Vue.use(VueX);
+
+export const store = new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
+});
--- src/views/Sign-up.vue
+++ src/views/Sign-up.vue
@@ -3,13 +3,13 @@
     <h1>Sign-up</h1>
     <v-container>
       <v-row>
-        <v-col>
+        <v-flex>
           <v-card>
             <v-card-text>
               <v-container>
-                <form @submit.prevent="addToStore">
+                <form @submit.prevent="addToUsers">
                   <v-row>
-                    <v-col>
+                    <v-flex>
                       <v-text-field v-model="name" label="Name" required type="name"></v-text-field>
                       <v-text-field v-model="email" label="E-mail" required type="email"></v-text-field>
                       <v-text-field v-model="password" label="Password" required type="password"></v-text-field>
@@ -22,13 +22,13 @@
                       ></v-text-field>
                       <v-btn type="submit">Submit</v-btn>
                       <v-btn type="clear">Clear</v-btn>
-                    </v-col>
+                    </v-flex>
                   </v-row>
                 </form>
               </v-container>
             </v-card-text>
           </v-card>
-        </v-col>
+        </v-flex>
       </v-row>
     </v-container>
   </div>
@@ -52,7 +52,7 @@ export default {
     },
   },
   methods: {
-    addToStore() {
+    addToUsers() {
       console.log({
         name: this.name,
         email: this.email,
