168c1b028ea8956eec26546957f24c1f25cd7caa
Adam Cassidy <adamcassidy011@gmail.com>
2020-08-26 09:42:48 -0400

    Make button disabled if form not valid
    All fields must not be empty to enable 'Submit'.

--- src/views/Post/CreatePost.vue
+++ src/views/Post/CreatePost.vue
@@ -10,10 +10,15 @@
                 <form @submit.prevent="addPost">
                   <v-row>
                     <v-flex>
-                      <v-text-field label="Title" single-line required></v-text-field>
-                      <v-textarea label="Context" auto-grow></v-textarea>
-                      <v-file-input label="Upload Image" outlined prepend-icon="image"></v-file-input>
-                      <v-btn type="submit">Submit</v-btn>
+                      <v-text-field v-model="title" label="Title" single-line required></v-text-field>
+                      <v-textarea v-model="context" label="Context" auto-grow></v-textarea>
+                      <v-file-input
+                        v-model="image"
+                        label="Upload Image"
+                        outlined
+                        prepend-icon="image"
+                      ></v-file-input>
+                      <v-btn type="submit" :disabled="!validForm">Submit</v-btn>
                       <v-btn type="clear">Clear</v-btn>
                     </v-flex>
                   </v-row>
@@ -43,6 +48,11 @@ export default {
         context: this.context,
         image: this.image,
       });
+    },
+  },
+  computed: {
+    validForm() {
+      return this.title != "" && this.context != "" && this.image != "";
     },
   },
 };
