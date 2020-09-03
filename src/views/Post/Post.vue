670c0e006a7e86480d40c3968b3e04ab77f0ff99
Adam Cassidy <adamcassidy011@gmail.com>
2020-09-02 22:28:06 -0400

    Fix tags for post images
    To correctly show post images

--- src/views/Post/Post.vue
+++ src/views/Post/Post.vue
@@ -1,27 +1,26 @@
-
 <template>
-  <v-div>
+<v-div>
     <v-container>
-      <v-row>
-        <v-flex>
-          <v-card>
-            <v-card-title class="headline" v-text="post.title"></v-card-title>
-            <v-card-subtitle v-text="post.date"></v-card-subtitle>
-            <v-card-media :src="post.src"></v-card-media>
-          </v-card>
-        </v-flex>
-      </v-row>
+        <v-row>
+            <v-flex>
+                <v-card>
+                    <v-card-title class="headline" v-text="post.title"></v-card-title>
+                    <v-card-subtitle v-text="post.date"></v-card-subtitle>
+                    <v-img :src="post.imageURL"></v-img>
+                </v-card>
+            </v-flex>
+        </v-row>
     </v-container>
-  </v-div>
+</v-div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
-  computed: {
-    post() {
-      return this.$store.getters.post(this.id);
+    props: ["id"],
+    computed: {
+        post() {
+            return this.$store.getters.post(this.id);
+        },
     },
-  },
 };
 </script>
--- src/views/Post/Posts.vue
+++ src/views/Post/Posts.vue
@@ -1,28 +1,31 @@
 <template>
-<v-div>
-    <v-container>
-        <v-row>
-            <v-flex v-for="post in posts" :key="post.id" cols="12">
-                <v-card>
+<v-container>
+    <v-row>
+        <v-flex v-for="post in posts" :key="post.id" cols="12">
+            <v-card>
+                <v-flex>
+                    <v-img :src="post.imageURL" width="200" height="200"></v-img>
                     <v-card-title class="headline" v-text="post.title"></v-card-title>
                     <v-card-subtitle v-text="post.date"></v-card-subtitle>
-                    <v-card-media :src="post.src"></v-card-media>
                     <v-card-actions>
-                        <v-btn @click="'./post/' + post.id">
+                        <v-btn :to="'./post/' + post.id">
                             <v-icon left>arrow_forward</v-icon>View
                         </v-btn>
                     </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-row>
-    </v-container>
-</v-div>
+                </v-flex>
+            </v-card>
+        </v-flex>
+    </v-row>
+</v-container>
 </template>
 
 <script>
 export default {
     computed: {
         posts() {
+            /*if (this.$store.getters.user) {
+                                              this.$store.dispatch("getPosts");
+                                          }*/
             return this.$store.getters.posts;
         },
     },
