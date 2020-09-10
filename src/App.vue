badcf5ff03c6fd048311d30feeb8f4a8e284ae55
Adam Cassidy <adamcassidy011@gmail.com>
2020-09-10 14:10:01 -0400

    Remove home|about nav
    Home can be accessed by clicking 'Second Opinion'.

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,6 @@
             </v-list-item-group>
         </v-list>
     </v-navigation-drawer>
-    <div id="nav">
-        <router-link to="/">Home</router-link>|
-        <router-link to="/about">About</router-link>
-    </div>
     <router-view />
 </div>
 </template>
@@ -127,19 +123,6 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
 }
 
 .custom-loader {
