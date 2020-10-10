e1f574cccefa0928577e6eedf8b737b3136e9d3c
Adam Cassidy <adamcassidy011@gmail.com>
2020-10-09 21:47:07 -0400

    Fix logo and logo margin.
    So the logo lines up with the rest of the name.

--- src/views/HomeProjects.vue
+++ src/views/HomeProjects.vue
@@ -2,7 +2,7 @@
   <v-container>
     <h1 style="font-size: 37px">
       <router-link to="/" tag="span" style="cursor: pointer; color: #701487">
-        <v-avatar class="mb-2"> <img src="../logo/logo.png" /> </v-avatar>nd
+        <v-avatar class="mb-1"> <img src="../logo/logo.png" /> </v-avatar>nd
         Opinion
       </router-link>
     </h1>
--- src/views/Missing.vue
+++ src/views/Missing.vue
@@ -1,11 +1,11 @@
 <template>
-<v-container>
+  <v-container>
     <h1 style="font-size: 37px">
-        <router-link to="/" tag="span" style="cursor: pointer; color: #701487">
-            <v-avatar class="mb-2"> <img src="../logo/logo.png" /> </v-avatar>nd
-            Opinion
-        </router-link>
+      <router-link to="/" tag="span" style="cursor: pointer; color: #701487">
+        <v-avatar class="mb-1"> <img src="../logo/logo.png" /> </v-avatar>nd
+        Opinion
+      </router-link>
     </h1>
     <h2>404 Page Not Found</h2>
-</v-container>
+  </v-container>
 </template>
