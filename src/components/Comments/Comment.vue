c8e0e57f725983d52564bb379dc42cdd3cd315a0
Adam Cassidy <adamcassidy011@gmail.com>
2020-10-14 18:12:34 -0400

    Turned discussion into async components
    So comments and replies aren't all loaded at the same time.

--- src/components/Comments/Comment.vue
+++ src/components/Comments/Comment.vue
@@ -1,117 +1,97 @@
 <template>
-  <v-container v-if="comment && !loading">
+<v-container v-if="comment && !loading">
     <v-row class="ms-2">
-      <v-avatar>
-        <img v-if="comment.photoURL" :src="comment.photoURL" />
-        <img v-else src="../../logo/logo.png" />
-      </v-avatar>
-      <v-col>
-        <h5 v-if="creator !== null && creator !== undefined">
-          {{ creator.userName }}
-        </h5>
-        <p style="font-size: 9px">{{ comment.date | date }}</p>
-      </v-col>
+        <v-avatar>
+            <img v-if="comment.photoURL" :src="comment.photoURL" />
+            <img v-else src="../../logo/logo.png" />
+        </v-avatar>
+        <v-col>
+            <h5 v-if="creator !== null && creator !== undefined">
+                {{ creator.userName }}
+            </h5>
+            <p style="font-size: 9px">{{ comment.date | date }}</p>
+        </v-col>
     </v-row>
     <v-row class="ms-4">
-      <p>{{ comment.comment }}</p>
+        <p>{{ comment.comment }}</p>
     </v-row>
-    <v-btn v-if="!replying && user && !userIsCreator" @click="replying = true"
-      >Reply</v-btn
-    >
-    <v-btn
-      v-if="
+    <v-btn v-if="!replying && user && !userIsCreator" @click="replying = true">Reply</v-btn>
+    <v-btn v-if="
         !loadReplies &&
         replies !== null &&
         replies !== undefined &&
         replies.length !== 0
-      "
-      @click="loadReplies = true"
-      >Load Replies</v-btn
-    >
+      " @click="loadReplies = true">Load Replies</v-btn>
     <v-btn v-if="loadReplies" @click="loadReplies = false">Close Replies</v-btn>
     <template v-if="userIsCreator">
-      <edit-comment-dialog :comment="comment"></edit-comment-dialog>
+        <edit-comment-dialog :comment="comment"></edit-comment-dialog>
     </template>
-    <comment-input
-      v-if="replying"
-      @send="(replyObj) => onSend(replyObj)"
-      @cancel="replying = false"
-      :post="post"
-      messageType="Reply"
-    ></comment-input>
+    <comment-input v-if="replying" @send="(replyObj) => onSend(replyObj)" @cancel="replying = false" :post="post" messageType="Reply"></comment-input>
     <div v-if="loadReplies" class="ml-4">
-      <reply
-        v-for="reply in replies"
-        :key="reply.id"
-        :id="reply.id"
-        :post="post"
-        :comment="comment"
-        :reply="reply"
-        :commentId="id"
-        @send="(replyObj) => onSend(replyObj)"
-      ></reply>
+        <reply v-for="reply in replies" :key="reply.id" :id="reply.id" :post="post" :comment="comment" :reply="reply" :commentId="id" @send="(replyObj) => onSend(replyObj)"></reply>
     </div>
-  </v-container>
+</v-container>
 </template>
 
 <script>
 export default {
-  props: {
-    post: {
-      type: Object,
-      required: true,
+    name: "AsyncComment",
+    props: {
+        post: {
+            type: Object,
+            required: true,
+        },
+        comment: {
+            type: Object,
+            required: true,
+        },
+        id: {
+            type: String,
+            required: true,
+        },
     },
-    comment: {
-      type: Object,
-      required: true,
+    data() {
+        return {
+            replying: false,
+            loadReplies: false,
+        };
     },
-    id: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      replying: false,
-      loadReplies: false,
-    };
-  },
-  computed: {
-    userIsAuthenticated() {
-      return (
-        this.$store.getters.user !== null &&
-        this.$store.getters.user !== undefined
-      );
-    },
-    userIsCreator() {
-      if (!this.userIsAuthenticated) {
-        return false;
-      }
+    computed: {
+        userIsAuthenticated() {
+            return (
+                this.$store.getters.user !== null &&
+                this.$store.getters.user !== undefined
+            );
+        },
+        userIsCreator() {
+            if (!this.userIsAuthenticated) {
+                return false;
+            }
 
-      return this.$store.getters.user.id === this.comment.creatorId;
+            return this.$store.getters.user.id === this.comment.creatorId;
+        },
+        loading() {
+            return this.$store.getters.loading;
+        },
+        replies() {
+            return this.$store.getters.replies(this.id);
+        },
+        user() {
+            return this.$store.getters.user;
+        },
+        creator() {
+            if (this.userIsCreator) {
+                return this.user;
+            } else {
+                return this.$store.getters.creator(this.comment.creatorId);
+            }
+        },
     },
-    loading() {
-      return this.$store.getters.loading;
+    methods: {
+        onSend(replyObj) {
+            replyObj.commentId = this.comment.id;
+            this.$emit("send", replyObj);
+        },
     },
-    replies() {
-      return this.$store.getters.replies(this.id);
-    },
-    user() {
-      return this.$store.getters.user;
-    },
-    creator() {
-      if (this.userIsCreator) {
-        return this.user;
-      } else {
-        return this.$store.getters.creator(this.comment.creatorId);
-      }
-    },
-  },
-  methods: {
-    onSend(replyObj) {
-      replyObj.commentId = this.comment.id;
-      this.$emit("send", replyObj);
-    },
-  },
 };
 </script>
--- src/components/Comments/CommentSection.vue
+++ src/components/Comments/CommentSection.vue
@@ -1,48 +1,40 @@
 <template>
-  <v-container v-if="post && !loading" class="text-start">
-    <comment-input
-      @send="(commentObj) => onSend(commentObj)"
-      :post="post"
-      messageType="Comment"
-    ></comment-input>
+<v-container v-if="post && !loading" class="text-start">
+    <comment-input @send="(commentObj) => onSend(commentObj)" :post="post" messageType="Comment"></comment-input>
     <v-divider class="my-4"></v-divider>
     <div v-if="comments">
-      <comment
-        v-for="comment in comments"
-        :key="comment.id"
-        :id="comment.id"
-        :post="post"
-        :comment="comment"
-        @send="(replyObj) => onReplySend(replyObj)"
-      ></comment>
+        <Comment v-for="comment in comments" :key="comment.id" :id="comment.id" :post="post" :comment="comment" @send="(replyObj) => onReplySend(replyObj)"></Comment>
     </div>
-  </v-container>
+</v-container>
 </template>
 
 <script>
 export default {
-  props: {
-    post: {
-      type: Object,
-      required: true,
+    props: {
+        post: {
+            type: Object,
+            required: true,
+        },
     },
-  },
-  computed: {
-    loading() {
-      return this.$store.getters.loading;
+    computed: {
+        loading() {
+            return this.$store.getters.loading;
+        },
+        comments() {
+            return this.$store.getters.comments(this.post.id);
+        },
     },
-    comments() {
-      return this.$store.getters.comments(this.post.id);
+    methods: {
+        onSend(commentObj) {
+            this.$store.dispatch("createComment", commentObj);
+        },
+        onReplySend(replyObj) {
+            replyObj.postId = this.post.id;
+            this.$store.dispatch("createReply", replyObj);
+        },
     },
-  },
-  methods: {
-    onSend(commentObj) {
-      this.$store.dispatch("createComment", commentObj);
+    components: {
+        Comment: () => import( /* webpackChunkName: "Comment" */ "./Comment.vue"),
     },
-    onReplySend(replyObj) {
-      replyObj.postId = this.post.id;
-      this.$store.dispatch("createReply", replyObj);
-    },
-  },
 };
 </script>
