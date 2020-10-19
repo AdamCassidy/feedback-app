362c84aec7fb8d6d3853afb50def23c725955ac5
Adam Cassidy <adamcassidy011@gmail.com>
2020-10-18 23:11:31 -0400

    Add Veutify Loader plugin.
    For better performance.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  chainWebpack: (config) => {
+    config.plugin("VuetifyLoaderPlugin").tap(() => [
+      {
+        match(originalTag, { kebabTag, camelTag }) {
+          if (kebabTag.startsWith("core-")) {
+            return [
+              camelTag,
+              `import ${camelTag} from '@/components/core/${camelTag.substring(
+                4
+              )}.vue'`,
+            ];
+          }
+        },
+      },
+    ]);
+  },
 };
