a38285a09e9a25abe17f478626bcb16fc459c9b0
Adam Cassidy <adamcassidy011@gmail.com>
2020-10-19 21:38:01 -0400

    Made footer asynchronous.
    Since it's not always being showed.

--- src/App.vue
+++ src/App.vue
@@ -63,24 +63,7 @@
             <router-view></router-view>
         </v-container>
     </v-main>
-    <v-footer>
-        <v-row justify="center" no-gutters>
-            <v-btn to="/" text>Home</v-btn>
-            <v-btn to="/about" text>About</v-btn>
-            <v-col cols="12">
-                <v-btn v-for="media in socialMedia" :key="media.icon" text><a :href="media.link" style="text-decoration: none">
-                        <v-icon color="black"> {{ media.icon }}</v-icon>
-                    </a>
-                </v-btn>
-            </v-col>
-            <v-col cols="12">
-                <strong style="font-size: 11px">
-                    <v-icon>copyright</v-icon>
-                    2020 - Second Opinion Forum. All Rights Reserved.
-                </strong>
-            </v-col>
-        </v-row>
-    </v-footer>
+    <Footer></Footer>
 </v-app>
 </template>
 
@@ -113,21 +96,6 @@ export default {
                     title: "Cooking",
                     link: "/cooking",
                     color: "silver",
-                },
-            ],
-            socialMedia: [{
-                    icon: "email",
-                    link: "mailto:2ndopinionforum@gmail.com",
-                },
-                {
-                    icon: "mdi-facebook",
-                    link: "https://www.facebook.com/2ndOpinionForum/",
-                    color: "#3b5998",
-                },
-                {
-                    icon: "mdi-twitter",
-                    link: "https://twitter.com/2ndForum",
-                    color: "#00acee",
                 },
             ],
         };
@@ -173,6 +141,13 @@ export default {
         onSignOut() {
             this.$store.dispatch("signOut");
         },
+    },
+    components: {
+        Footer: () =>
+            import(
+                /* webpackChunkName: "Footer" */
+                "./components/Footer.vue"
+            ),
     },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-footer>
+    <v-row justify="center" no-gutters>
+      <v-btn to="/" text>Home</v-btn>
+      <v-btn to="/about" text>About</v-btn>
+      <v-col cols="12">
+        <v-btn v-for="media in socialMedia" :key="media.icon" text
+          ><a :href="media.link" style="text-decoration: none">
+            <v-icon color="black"> {{ media.icon }}</v-icon>
+          </a>
+        </v-btn>
+      </v-col>
+      <v-col cols="12">
+        <strong style="font-size: 11px">
+          <v-icon>copyright</v-icon>
+          2020 - Second Opinion Forum. All Rights Reserved.
+        </strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      socialMedia: [
+        {
+          icon: "email",
+          link: "mailto:2ndopinionforum@gmail.com",
+        },
+        {
+          icon: "mdi-facebook",
+          link: "https://www.facebook.com/2ndOpinionForum/",
+          color: "#3b5998",
+        },
+        {
+          icon: "mdi-twitter",
+          link: "https://twitter.com/2ndForum",
+          color: "#00acee",
+        },
+      ],
+    };
+  },
+};
+</script>
