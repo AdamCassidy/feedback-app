e74294750d00aa7ceb0ac1c480e806290606b065
Adam Cassidy <adamcassidy011@gmail.com>
2020-10-24 14:21:52 -0400

    Remove gap and change xs layout
    So there's no gap between images and that 'share' is on top of the
    buttons for xs.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,116 +1,134 @@
 <template>
-<div>
+  <div>
     <h1 style="font-size: 37px">
-        <router-link to="/" tag="span" style="cursor: pointer; color: #701487">
-            <v-avatar class="mb-1"> <img src="../logo/logo.png" /> </v-avatar>nd
-            Opinion
-        </router-link>
+      <router-link to="/" tag="span" style="cursor: pointer; color: #701487">
+        <v-avatar class="mb-1"> <img src="../logo/logo.png" /> </v-avatar>nd
+        Opinion
+      </router-link>
     </h1>
     <v-row v-if="loading">
-        <v-col>
-            <v-progress-circular v-if="loading" :size="50" indeterminate color="primary"></v-progress-circular>
-        </v-col>
+      <v-col>
+        <v-progress-circular
+          v-if="loading"
+          :size="50"
+          indeterminate
+          color="primary"
+        ></v-progress-circular>
+      </v-col>
     </v-row>
     <v-parallax v-else class="mt-4" src="../assets/sunglassesAbout.jpeg">
-        <v-row></v-row>
-        <v-row class="mb-20" justify="center">
-            <v-col class="ms-7">
-                <h2 class="white--text">See through a different lens<br /></h2>
-                <p class="white--text" style="font-size: 14px">
-                    A forum to gather opinions on whatever you post<br />
-                </p>
-                <v-btn v-if="!userIsAuthenticated" class="secondary" to="/signup">Join</v-btn>
-            </v-col>
-            <v-col></v-col>
-        </v-row>
-        <v-row></v-row>
-    </v-parallax>
-    <v-row v-if="!loading" justify="center">
-        <v-col cols="12">
-            <v-banner color="gradient">
-                <v-row justify="center" class="me-1">
-                    <h3 class="white--text mt-6 ms-1">Share:&nbsp;&nbsp;</h3>
-
-                    <v-btn v-for="item in shareItems" color="secondary" :key="item.key" fab class="ma-2" transistion="scroll - y - reverse - transition">
-                        <a :href="item.link" style="text-decoration: none">
-                            <v-icon color="white">{{ item.icon }}</v-icon>
-                        </a>
-                    </v-btn>
-                </v-row>
-            </v-banner>
+      <v-row></v-row>
+      <v-row class="mb-20" justify="center">
+        <v-col class="ms-7">
+          <h2 class="white--text">See through a different lens<br /></h2>
+          <p class="white--text" style="font-size: 14px">
+            A forum to gather opinions on whatever you post<br />
+          </p>
+          <v-btn v-if="!userIsAuthenticated" class="secondary" to="/signup"
+            >Join</v-btn
+          >
         </v-col>
-    </v-row>
-</div>
+        <v-col></v-col>
+      </v-row>
+      <v-row></v-row>
+    </v-parallax>
+    <v-banner color="gradient">
+      <h3 v-if="$vuetify.breakpoint.xs" class="white--text mt-6 ms-1">
+        Share:&nbsp;&nbsp;
+      </h3>
+
+      <v-row justify="center" class="me-1">
+        <h3 v-if="!$vuetify.breakpoint.xs" class="white--text mt-6 ms-1">
+          Share:&nbsp;&nbsp;
+        </h3>
+
+        <v-btn
+          v-for="item in shareItems"
+          color="secondary"
+          :key="item.key"
+          fab
+          class="ma-2"
+          transistion="scroll - y - reverse - transition"
+        >
+          <a :href="item.link" style="text-decoration: none">
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </a>
+        </v-btn>
+      </v-row>
+    </v-banner>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => {
-        return {
-            shareItems: [{
-                    icon: "mdi-facebook",
-                    link: "https://www.facebook.com/2ndOpinionForum/",
-                    color: "#3b5998",
-                },
-                {
-                    icon: "mdi-twitter",
-                    link: "https://twitter.com/intent/tweet?text=Check%20out%20this%20forum.&url=https%3a%2f%2fsecondopinionforum.com",
-                    color: "#00acee",
-                },
-                {
-                    icon: "mdi-reddit",
-                    link: "https://www.reddit.com/submit?title=Check%20out%20this%20forum.%20&url=https%3A%2F%2Fsecondopinionforum.com",
-                    color: "#FF4500",
-                },
-                {
-                    icon: "mdi-whatsapp",
-                    link: "https://wa.me/?text=Check%20out%20this%20forum.%20https%3A%2F%2Fsecondopinionforum.com",
-                    color: "#25d366",
-                },
-            ],
-        };
-    },
-    computed: {
-        loading() {
-            return this.$store.getters.loading;
+  data: () => {
+    return {
+      shareItems: [
+        {
+          icon: "mdi-facebook",
+          link: "https://www.facebook.com/2ndOpinionForum/",
+          color: "#3b5998",
         },
-        userIsAuthenticated() {
-            return (
-                this.$store.getters.user !== null &&
-                this.$store.getters.user !== undefined
-            );
+        {
+          icon: "mdi-twitter",
+          link:
+            "https://twitter.com/intent/tweet?text=Check%20out%20this%20forum.&url=https%3a%2f%2fsecondopinionforum.com",
+          color: "#00acee",
         },
+        {
+          icon: "mdi-reddit",
+          link:
+            "https://www.reddit.com/submit?title=Check%20out%20this%20forum.%20&url=https%3A%2F%2Fsecondopinionforum.com",
+          color: "#FF4500",
+        },
+        {
+          icon: "mdi-whatsapp",
+          link:
+            "https://wa.me/?text=Check%20out%20this%20forum.%20https%3A%2F%2Fsecondopinionforum.com",
+          color: "#25d366",
+        },
+      ],
+    };
+  },
+  computed: {
+    loading() {
+      return this.$store.getters.loading;
     },
-    created() {
-        this.$store.dispatch("getAboutPicURL");
+    userIsAuthenticated() {
+      return (
+        this.$store.getters.user !== null &&
+        this.$store.getters.user !== undefined
+      );
     },
+  },
+  created() {
+    this.$store.dispatch("getAboutPicURL");
+  },
 };
 </script>
 
 <style lang="scss">
 @mixin gradient($start-color, $end-color, $orientation) {
-    background: $start-color;
+  background: $start-color;
 
-    @if $orientation=="vertical" {
-        background: -webkit-linear-gradient(top, $start-color, $end-color);
-        background: linear-gradient(to bottom, $start-color, $end-color);
-    }
-
-    @else if $orientation=="horizontal" {
-        background: -webkit-linear-gradient(left, $start-color, $end-color);
-        background: linear-gradient(to right, $start-color, $end-color);
-    }
-
-    @else {
-        background: -webkit-radial-gradient(center,
-                ellipse cover,
-                $start-color,
-                $end-color);
-        background: radial-gradient(ellipse at center, $start-color, $end-color);
-    }
+  @if $orientation== "vertical" {
+    background: -webkit-linear-gradient(top, $start-color, $end-color);
+    background: linear-gradient(to bottom, $start-color, $end-color);
+  } @else if $orientation== "horizontal" {
+    background: -webkit-linear-gradient(left, $start-color, $end-color);
+    background: linear-gradient(to right, $start-color, $end-color);
+  } @else {
+    background: -webkit-radial-gradient(
+      center,
+      ellipse cover,
+      $start-color,
+      $end-color
+    );
+    background: radial-gradient(ellipse at center, $start-color, $end-color);
+  }
 }
 
 .gradient {
-    @include gradient(#701487, #bbc919, horizontal);
+  @include gradient(#701487, #bbc919, horizontal);
 }
 </style>
